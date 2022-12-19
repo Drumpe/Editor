7d61c9d664f7285292547959c0c315f236010a42
Drumpe <96122414+Drumpe@users.noreply.github.com>
2022-12-19 12:11:41 +0100

    Add project files.

---
+++ Editor/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Editor
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editor));
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelOrd = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelTecken = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelTeckenUM = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelRader = new System.Windows.Forms.ToolStripStatusLabel();
+            this.flowLayoutPanel.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.Controls.Add(this.menuStrip);
+            resources.ApplyResources(this.flowLayoutPanel, "flowLayoutPanel");
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip, "menuStrip");
+            this.menuStrip.Name = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.toolStripSeparator,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            // 
+            // newToolStripMenuItem
+            // 
+            resources.ApplyResources(this.newToolStripMenuItem, "newToolStripMenuItem");
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            resources.ApplyResources(this.openToolStripMenuItem, "openToolStripMenuItem");
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            resources.ApplyResources(this.toolStripSeparator, "toolStripSeparator");
+            // 
+            // saveToolStripMenuItem
+            // 
+            resources.ApplyResources(this.saveToolStripMenuItem, "saveToolStripMenuItem");
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            resources.ApplyResources(this.saveAsToolStripMenuItem, "saveAsToolStripMenuItem");
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator5,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            resources.ApplyResources(this.toolStripSeparator5, "toolStripSeparator5");
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // richTextBox
+            // 
+            resources.ApplyResources(this.richTextBox, "richTextBox");
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabelOrd,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabelTecken,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabelTeckenUM,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabelRader});
+            resources.ApplyResources(this.statusStrip, "statusStrip");
+            this.statusStrip.Name = "statusStrip";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+            // 
+            // toolStripStatusLabelOrd
+            // 
+            this.toolStripStatusLabelOrd.Name = "toolStripStatusLabelOrd";
+            resources.ApplyResources(this.toolStripStatusLabelOrd, "toolStripStatusLabelOrd");
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            resources.ApplyResources(this.toolStripStatusLabel2, "toolStripStatusLabel2");
+            // 
+            // toolStripStatusLabelTecken
+            // 
+            this.toolStripStatusLabelTecken.Name = "toolStripStatusLabelTecken";
+            resources.ApplyResources(this.toolStripStatusLabelTecken, "toolStripStatusLabelTecken");
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            resources.ApplyResources(this.toolStripStatusLabel3, "toolStripStatusLabel3");
+            // 
+            // toolStripStatusLabelTeckenUM
+            // 
+            this.toolStripStatusLabelTeckenUM.Name = "toolStripStatusLabelTeckenUM";
+            resources.ApplyResources(this.toolStripStatusLabelTeckenUM, "toolStripStatusLabelTeckenUM");
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            resources.ApplyResources(this.toolStripStatusLabel4, "toolStripStatusLabel4");
+            // 
+            // toolStripStatusLabelRader
+            // 
+            this.toolStripStatusLabelRader.Name = "toolStripStatusLabelRader";
+            resources.ApplyResources(this.toolStripStatusLabelRader, "toolStripStatusLabelRader");
+            // 
+            // Editor
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Editor";
+            this.flowLayoutPanel.ResumeLayout(false);
+            this.flowLayoutPanel.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelOrd;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelTecken;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelTeckenUM;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelRader;
+    }
+}
+
---
+++ Editor/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    public partial class Editor : Form
+    {
+        private const string EDITORNAME = " - Editor";
+        private const string NAMELESS = "Namnlös";
+        private string fileName = NAMELESS;
+        private string fileExtension = "";
+        private bool textChanged = false;
+        private bool oppnadViaDragAndDrop = false;
+        private string allText = ""; //Sparar richTextBox.Text här, för att kunna spara när Form är stängd
+        //Hittar inte möjligheten att fånga en eventhandler innan Form stängs så sparar text i allText
+
+        public Editor()
+        {
+            InitializeComponent();
+
+            // Sets the control to allow drops, and then adds the necessary event handlers.
+            richTextBox.AllowDrop = true;
+            richTextBox.DragDrop += RichTextBox_DragDrop;
+
+            // Handle the ApplicationExit event to know when the application is exiting.
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+            this.Text = fileName + EDITORNAME;
+        }
+
+        //Om exit med X
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            if (textChanged)
+            {
+                var result = MessageBox.Show(
+                    $"Vill du spara ändringarna för {fileName}",
+                    "Editor Spara?", MessageBoxButtons.YesNo
+                    );
+                if (result == DialogResult.Yes)
+                {
+                    //Save file
+                    SparaMedEllerUtanDialog();
+                }
+            }
+            Application.Exit();
+        }
+
+        //File - New
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SparaEller())
+            {
+                richTextBox.Clear();
+                fileName = NAMELESS;
+                this.Text = fileName + EDITORNAME;
+                textChanged = false;
+            }
+            UpdateStatusBar();
+        }
+
+        /// <summary>
+        /// Sparar fil med en dialogruta
+        /// </summary>
+        private void SparaFilMedDialog()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Textfil|*.txt|Alla filer|*.*";
+            var saveFileResult = saveFileDialog.ShowDialog();
+            if (saveFileResult == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, allText);
+                fileName = Path.GetFileName(saveFileDialog.FileName);
+                fileExtension = Path.GetExtension(saveFileDialog.FileName);
+                textChanged = false;
+                this.Text = fileName + EDITORNAME;
+            }
+        }
+
+        /// <summary>
+        /// Spara fil utan dialog
+        /// </summary>
+        private void SparaFilUtanDialog()
+        {
+            File.WriteAllText(fileExtension, allText);
+            textChanged = false;
+            this.Text = fileName + EDITORNAME;
+        }
+
+        /// <summary>
+        /// Sparar fil med eller utan dialog
+        /// </summary>
+        private void SparaMedEllerUtanDialog()
+        {
+            if (textChanged)
+            {
+                if (fileName == NAMELESS)
+                {
+                    SparaFilMedDialog();
+                }
+                else
+                {
+                    SparaFilUtanDialog();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Kontrollerar om filen skall sparas med eller utan dialog
+        /// och sparar filen med eller utan dialog.
+        /// </summary>
+        /// <returns>false - om åtgärd skall avbrytas</returns>
+        private bool SparaEller()
+        {
+            if (textChanged)
+            {
+                var result = MessageBox.Show(
+                            $"Vill du spara ändringarna för {fileName}",
+                            "Editor Spara?", MessageBoxButtons.YesNoCancel
+                             );
+                if (result == DialogResult.Yes)
+                {
+                    //Save file
+                    if (fileName == NAMELESS)
+                    {
+                        SparaFilMedDialog();
+                    }
+                    else
+                    {
+                        SparaFilUtanDialog();
+                    }
+                    return true;
+                }
+                else if (result == DialogResult.No)
+                {
+                    //Don't save 
+                    return true;
+                }
+                else
+                {
+                    //Don't do anything!
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Räknar antalet ord i str
+        /// </summary>
+        /// <param name="str"> sträng</param>
+        /// <returns>Antal ord</returns>
+        private int WordCount(string str)
+        {
+            int count = 0;
+            string pattern = "[^\\w]";
+            string[] words = null;
+            words = Regex.Split(str, pattern, RegexOptions.IgnoreCase);
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Räknar antalet förekomna t i str
+        /// </summary>
+        /// <param name="str">Sträng</param>
+        /// <param name="t">Tecken</param>
+        /// <returns>Antalet t i str</returns>
+        private int CharCount(string str, char t)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c.Equals(t))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Updaterar statusbaren
+        /// </summary>
+        private void UpdateStatusBar()
+        {
+            int nrOfChars = richTextBox.TextLength - richTextBox.Text.Count(char.IsControl);
+            toolStripStatusLabelTecken.Text = nrOfChars.ToString();
+            toolStripStatusLabelOrd.Text = WordCount(richTextBox.Text).ToString();
+            toolStripStatusLabelRader.Text = (CharCount(richTextBox.Text, '\n') + 1).ToString();
+            toolStripStatusLabelTeckenUM.Text = (nrOfChars - CharCount(richTextBox.Text, ' ')).ToString();
+        }
+
+        //Om texten ändras i textboxen
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (oppnadViaDragAndDrop) //När fil öppnas via D&D
+            {
+                oppnadViaDragAndDrop = false;
+            }
+            else
+            {
+                textChanged = true;
+                this.Text = "*" + fileName + EDITORNAME; //Ändring av fil markeras med * framför filnamn
+            }
+
+            //Spara för stängning med X (Finns det bättre sätt?)
+            allText = richTextBox.Text;
+            UpdateStatusBar();
+        }
+
+        //File - Save
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Spara bara om texten är ändrad
+            SparaMedEllerUtanDialog();
+        }
+
+        //File - Save As
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SparaFilMedDialog();
+        }
+
+        //Edit - Open
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SparaEller())
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Textfil|*.txt|Alla filer|*.*";
+                var openFileResult = openFileDialog.ShowDialog();
+                if (openFileResult == DialogResult.OK)
+                {
+                    richTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+                    fileName = openFileDialog.SafeFileName;
+                    fileExtension = openFileDialog.FileName;
+                    textChanged = false;
+                    this.Text = fileName + EDITORNAME;
+                }
+            }
+            UpdateStatusBar();
+        }
+
+        //File - Exit
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SparaEller())
+            {
+                //Exit
+                Application.Exit();
+            }
+        }
+
+        private void exit()
+        {
+            if (SparaEller())
+            {
+                //Exit
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Hanterar D&D event
+        /// Kan lägga till text - läggs till där det släpps
+        /// Hanterar filer - ctrl - shift - none
+        /// Fungerar bara som fristående program, inte när jag kör debug is VS
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RichTextBox_DragDrop(object sender, DragEventArgs e)
+        {
+            // Paste the text into the RichTextBox where at selection location.
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                richTextBox.SelectedText = e.Data.GetData("System.String", true).ToString();
+            }
+            else if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                if (e.Data != null)
+                {
+                    var data = e.Data.GetData(DataFormats.FileDrop);
+                    var filnamn = data as string[];
+                    if (filnamn.Length > 0)
+                    {
+                        if ((e.KeyState & 4) == 4) // shift
+                        {
+                            richTextBox.SelectedText = File.ReadAllText(filnamn[0]);
+                        }
+                        else if ((e.KeyState & 8) == 8) //ctrl
+                        {
+                            richTextBox.Text += File.ReadAllText(filnamn[0]);
+                        }
+                        else if ((e.KeyState & 0) == 0) //none
+                        {
+                            if (SparaEller())
+                            {
+                                richTextBox.Text = File.ReadAllText(filnamn[0]);
+                                fileName = Path.GetFileName(filnamn[0]);
+                                textChanged = false;
+                                this.Text = fileName + EDITORNAME;
+                                oppnadViaDragAndDrop = true;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Editor\nA program by Ola P", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
